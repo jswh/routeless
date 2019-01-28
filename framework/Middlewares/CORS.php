3cdf7eb3d7f5982b6dd2ffef8a0608a1ca8a9e89
jswh@jswh.me <jswh@jswh.me>
2019-01-28 17:59:09 +0800

    add redis get function

--- framework/Middlewares/CORS.php
+++ framework/Middlewares/CORS.php
@@ -11,9 +11,9 @@ use Routeless\Services\Cfg;
 class CORS extends MiddleWare {
     public function handle() {
         $cfg = Cfg::get('app.cors') ?: [];
-        $this->response->header('Access-Control-Allow-Origin', $cfg['origins'] ?: '*');
-        $this->response->header('Access-Control-Allow-Headers', $cfg['headers'] ?: 'Authorization, Content-Type');
-        $this->response->header('Access-Control-Allow-Methods', $cfg['methods'] ?: '*');
+        $this->response->header('Access-Control-Allow-Origin', $cfg['origins'] ?? '*');
+        $this->response->header('Access-Control-Allow-Headers', $cfg['headers'] ?? 'Authorization, Content-Type');
+        $this->response->header('Access-Control-Allow-Methods', $cfg['methods'] ?? '*');
         if ($this->request->method() == Request::HTTP_METHOD_OPTIONS) {
             $this->response->send();
             die;
