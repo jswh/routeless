081ee2dbc8e66a728b74a2eb0371dfde79c6ea5e
jswh <jswh@jswh.me>
2019-03-14 10:53:55 +0800

    change env loader

--- framework/Core/Application.php
+++ framework/Core/Application.php
@@ -16,6 +16,7 @@ class Application
 
     public function __construct($configPath)
     {
+        Dotenv\Dotenv::create($configPath)->load();
         (new Dotenv($configPath))->load();
         $this->config = new Config($configPath);
         static::$app = $this;
